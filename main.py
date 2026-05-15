6c15e9d8456c021f12be9c26f00a98d19286122a
mj-caro <mj.caro@uniandes.edu.co>
2026-05-15 18:32:29 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from datetime import datetime
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+#os.environ para despliegue. Descomente cuando ya probó todo local.
+client = MongoClient(os.environ["MONGO_URI"])
+
+# client = MongoClient("mongodb://<usuario>:<contraseña>@157.253.236.88:8087")
+
+db = client["ISIS2304A14202610"]
+
+
+@app.get("/")
+def inicio():
+    return {"estado": "API funcionando correctamente"}
+
+@app.get('/bares/{bar_id}/comentarios')
+def get_comentarios(bar_id: int):
+    comentarios = list(db["comentarios"].find({"bar_id": bar_id}, {"_id": 0}))  
+    return comentarios
+
+@app.post('/bares/{bar_id}/comentarios')
+def post_comentario(bar_id: int, datos: dict):
+    datos['bar_id'] = bar_id
+    datos['fecha']  = datetime.now().isoformat()
+
+    db["comentarios"].insert_one(datos)
+    
+    return {'mensaje': 'Comentario guardado'}
+
+
+@app.get('/bares/{bar_id}/eventos')
+def get_eventos(bar_id: int):
+    eventos = list(db["eventos"].find({"bar_id": bar_id}, {"_id": 0}))  
+    return eventos
+
+@app.post('/bares/{bar_id}/eventos')
+def post_eventos(bar_id: int, datos: dict):
+    datos['bar_id'] = bar_id
+    datos['fecha']  = datetime.now().isoformat()
+    
+    evento = {
+        "_id": db["eventos"].count_documents({}) + 1,
+        "bar_id": bar_id,
+        "nombre": datos["nombre"],
+        "fecha": datetime.now().isoformat(),
+        "descripcion": datos["descripcion"],
+        "detalles": datos["detalles"]
+    }
+
+    db["comentarios"].insert_one(evento)
+    
+    return {'mensaje': 'Comentario guardado'}
